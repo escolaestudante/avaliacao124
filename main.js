1104b80059384cf9d99ad1cbd2d7cb35c2ff59c9
escolaestudante <105922788+escolaestudante@users.noreply.github.com>
2024-04-16 08:10:34 -0300

    tyyu

--- main.js
+++ main.js
@@ -11,5 +11,8 @@ for( let i = 0; i < botoes.length; i++){
         textos[i].classList.add("ativo");
     }
 }
-const contadores = document.querySelectorAll(".contador")
-const tempoObjetivo1 = new Date("2024-12-01")
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-12-01");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
